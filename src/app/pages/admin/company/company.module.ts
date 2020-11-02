6cde6ac0f89e02e5a2261756d995686adeb74148
JOSECONDORI5 <jose.condori5@unmsm.edu.pe>
2020-11-02 11:51:28 -0500

    Component driver created

--- src/app/pages/admin/company/company.module.ts
+++ src/app/pages/admin/company/company.module.ts
@@ -1,15 +1,22 @@
 
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { CompanyFormComponent } from './company-form/company-form.component';
 import { CompanyListComponent } from './company-list/company-list.component';
+import { DriverComponent } from './driver/driver.component';
+import { MaterialModule } from '../../../_material/material.module';
 
-
+const routes: Routes = [
+  { path: '', component: DriverComponent }
+];
 
 @NgModule({
-  declarations: [ CompanyFormComponent, CompanyListComponent],
+  declarations: [ CompanyFormComponent, CompanyListComponent, DriverComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(routes),
+    MaterialModule
   ]
 })
 export class CompanyModule { }
