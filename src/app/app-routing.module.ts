fa1f5abff7f16f10578510990942a84f3d78b53c
Fernando Chavez <alvaro1221x@gmail.com>
2020-11-08 23:59:28 -0500

    implementacion card de productos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,13 +13,16 @@ const routes: Routes = [
   },
   {
     path: 'product', loadChildren: () => import('./pages/admin/product/product.module').then(m => m.ProductModule)
+  },
+  {
+    path: 'order', loadChildren: () => import('./pages/admin/order/order.module').then(m => m.OrderModule)
   }
   ]},
   {
     path: 'auth', loadChildren: () => import('./pages/authorization/authorization.module').then(m => m.AuthorizationModule)
   },
 
-  { path: '**', pathMatch: 'full', redirectTo: '/auth' },
+  { path: '**', pathMatch: 'full', redirectTo: '/order/store' },
 ];
 
 @NgModule({
