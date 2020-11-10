c1605f492d224a8f8e0a269b6da7a7a6d44b6124
Fernando Chavez <alvaro1221x@gmail.com>
2020-11-09 23:26:09 -0500

    avance order service

--- src/app/_service/order.service.ts
+++ src/app/_service/order.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { OrderBean } from '../_model/OrderBean';
+import { OrderDetailBean } from '../_model/OrderDetailBean';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OrderService {
-  Order
+  newOrder:OrderBean;
+  orderDetailList:OrderDetailBean[]=[];
   constructor() { }
+
+
+  sendNewOrder(address:string,reference:string,phone:string){
+    this.newOrder.address=address;
+    this.newOrder.reference=reference;
+    this.newOrder.phone=phone;
+    this.newOrder.orderDetailList=this.orderDetailList;
+    console.log(this.newOrder);
+    this.newOrder = new OrderBean();
+    this.orderDetailList=[];
+  }
 }
