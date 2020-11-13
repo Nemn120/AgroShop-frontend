6485f66250b864fa016f263bb104c234663a8a1c
Osti Katriel <katriel181920@gmail.com>
2020-11-13 00:06:28 -0500

    crud basico de categorias

---
+++ src/app/pages/admin/gestion/categories/categories-form/categories-form.component.ts
@@ -0,0 +1,74 @@
+import { Component, OnInit ,Inject} from '@angular/core';
+
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { DomSanitizer } from '@angular/platform-browser';
+import { CategoryProductBean } from '../../../../../_model/CategoryProductBean';
+import { RestService } from 'src/app/_service/rest.service';
+import { SharedService } from 'src/app/_service/shared.service';
+
+@Component({
+  selector: 'app-categories-form',
+  templateUrl: './categories-form.component.html',
+  styleUrls: ['./categories-form.component.scss']
+})
+export class CategoriesFormComponent implements OnInit {
+
+  titulo:string="Nuevo ";
+  categorySelect: CategoryProductBean;
+
+
+  constructor(
+    private restService: RestService,
+    private sharedService:SharedService,
+
+    public dialogRef: MatDialogRef<CategoriesFormComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: CategoryProductBean,
+  ) { }
+
+  ngOnInit(): void {
+   
+   
+    this.categorySelect = new CategoryProductBean();
+    this.categorySelect.userCreateId = this.sharedService.userSession.id;
+    
+    console.log('categorySelect: ',this.categorySelect);
+   
+    if (this.data.id > 0) {
+      this.categorySelect.id = this.data.id;
+      this.categorySelect.name = this.data.name;
+      this.categorySelect.description = this.data.description;
+
+      this.titulo="Actualizar ";
+    }
+
+
+  }
+  save() {
+  
+    let param = {
+      data: this.categorySelect,
+    }
+
+ 
+    this.restService.requestApiRestData('categoryproduct/scp', param,).subscribe(result => {
+      console.log('result: ',result);
+
+      if (this.categorySelect.id) {
+        this.restService.messageChange.next({ message: 'Categoria actualizado con exito!', action: "Update" });
+      }
+      else {
+        this.restService.messageChange.next({ message: 'Categoria agregado con exito!', action: "Create" });
+      }
+
+    }, error => {
+      console.log("Error al agregar categoria! ", error);
+      this.restService.message('Error al agregar categoria!', 'Error');
+    });
+    this.closeDialog();
+  }
+  
+  closeDialog(): void {
+    this.dialogRef.close();
+  }
+
+}
---
+++ src/app/pages/admin/gestion/categories/categories-view/categories-view.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit,Inject } from '@angular/core';
+
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { DomSanitizer } from '@angular/platform-browser';
+import { RestService } from '../../../../../_service/rest.service';
+import { ProductViewComponent } from '../../../product/product-view/product-view.component';
+import { CategoryProductBean } from './../../../../../_model/CategoryProductBean';
+
+@Component({
+  selector: 'app-categories-view',
+  templateUrl: './categories-view.component.html',
+  styleUrls: ['./categories-view.component.scss']
+})
+export class CategoriesViewComponent implements OnInit {
+
+  categorySelect: CategoryProductBean;
+
+  imagenData: any;
+  imagenEstado: boolean = false;
+  selectedFiles: FileList;
+  currentFileUpload: File;
+  labelFile: string;
+
+  constructor(
+    private restService: RestService,
+    public dialogRef: MatDialogRef<ProductViewComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: CategoryProductBean,
+
+    private sanitization: DomSanitizer,
+
+  ) { }
+
+  ngOnInit(): void {
+  
+  }
+
+
+  closeDialog(): void {
+    this.dialogRef.close();
+  }
+
+
+}
