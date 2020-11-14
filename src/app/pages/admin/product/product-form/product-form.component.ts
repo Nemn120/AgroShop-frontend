7a6a857fb52d45c8b3e92757d7ee3a24a1b4c9e4
Osti Katriel <katriel181920@gmail.com>
2020-11-14 11:20:55 -0500

    estilos generales

--- src/app/pages/admin/product/product-form/product-form.component.ts
+++ src/app/pages/admin/product/product-form/product-form.component.ts
@@ -14,6 +14,7 @@ import { SharedService } from '../../../../_service/shared.service';
 })
 export class ProductFormComponent implements OnInit {
 
+  selected = 'option2';
   titulo:string="Nuevo ";
 
   productSelect: ProductBean;
