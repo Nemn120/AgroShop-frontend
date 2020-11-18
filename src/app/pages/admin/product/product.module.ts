32cdd5b0cee69a039472a792601b5ec89486001b
Fernando Chavez <alvaro1221x@gmail.com>
2020-11-17 23:27:58 -0500

    merge

--- src/app/pages/admin/product/product.module.ts
+++ src/app/pages/admin/product/product.module.ts
@@ -5,14 +5,13 @@ import { ProductListComponent } from './product-list/product-list.component';
 import { RouterModule, Routes } from '@angular/router';
 import { MaterialModule } from 'src/app/_material/material.module';
 import { PanelAdminModule } from '../panel-admin/panel-admin.module';
-import { WelcomeComponent } from '../panel-admin/welcome/welcome.component';
 import { ProductFormComponent } from './product-form/product-form.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ProductViewComponent } from './product-view/product-view.component';
 import { CategoriesListComponent } from '../gestion/categories/categories-list/categories-list.component';
 import { CategoriesViewComponent } from '../gestion/categories/categories-view/categories-view.component';
-import { ProductsSalesListComponent } from '../gestion/products-sales/products-sales-list/products-sales-list.component';
 import { ProductsSalesFormComponent } from '../gestion/products-sales/products-sales-form/products-sales-form.component';
+import { ProductsSalesListComponent } from '../gestion/products-sales/products-sales-list/products-sales-list.component';
 import { ProductsSalesViewComponent } from '../gestion/products-sales/products-sales-view/products-sales-view.component';
 const routes: Routes = [
   
@@ -25,7 +24,7 @@ const routes: Routes = [
    //{path: 'list', component: CategoriesListComponent},//gestion categorias
   
    //temporal
-   {path: 'sales', component: ProductsSalesListComponent}//gestion productos venta
+   {path: 'sales', component: ProductsSalesListComponent}
   
 ];
 
