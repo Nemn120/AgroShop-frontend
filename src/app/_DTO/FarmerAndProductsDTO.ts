c8db6861732ee8d45968c6f5d12fa33fcf3cf13f
felipe jimenez <new_win2100@hotmail.com>
2020-11-23 01:50:38 -0500

    titulo farmer busqueda

--- src/app/_DTO/FarmerAndProductsDTO.ts
+++ src/app/_DTO/FarmerAndProductsDTO.ts
@@ -2,6 +2,6 @@ import {FarmerBean} from '../_model/FarmerBean';
 import {ProductSalesBean} from '../_model/ProductSalesBean';
 
 export class FarmerAndProductsDTO{
-    _farmer: string;
+    _farmer: any;
     _listOfProductsShowed: any;
 }
