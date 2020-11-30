f98d2562a11de0f6a0a50d3cf46c127b3bf3a397
Jordy <jordy.sarmiento@unmsm.edu.pe>
2020-11-30 14:02:06 -0500

    cambio width paginator

--- src/app/_model/JobOfferByFields.ts
+++ src/app/_model/JobOfferByFields.ts
@@ -2,8 +2,6 @@ export class JobOfferByFields{
     status: string;
     departmentIni: string;
     departmentFin: string;
-    dateIni: Date;
-    dateFin: Date;
     weightIni: number;
     weightFin: number;
     priceIni: number;
