8bc1ba219be9234bb7f50bf8cc5a75022f9ca528
Jordy <jordy.sarmiento@unmsm.edu.pe>
2020-11-30 13:02:26 -0500

    adding paginator

--- src/app/pages/admin/job-offer/offer/offer.component.ts
+++ src/app/pages/admin/job-offer/offer/offer.component.ts
@@ -1,21 +1,127 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit, ViewChild } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { Observable } from 'rxjs';
 import { OfferDetailComponent } from '../offer-detail/offer-detail.component';
 
+export interface JobOffer{
+  title: string;
+  description: string;
+  requeriments: string;
+  date: string;
+}
+
+const DATA: JobOffer[] = [
+  {
+    title: 'Se buscan Felipes (Esclavo)',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum amet, facere officia est maxime dolore libero sint perspiciatis suscipit consequatur! Fuga voluptatum fugiat saepe impedit? Veritatis voluptatum voluptas deserunt ut.',
+    requeriments: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt aliquam placeat aliquid quam voluptates illum vitae expedita. Dolor ad repellendus ab, corporis qui saepe quisquam, fugit ipsa odit numquam placeat.',
+    date: '666 de marzo del 2666'
+  },
+  {
+    title: 'Se buscan Felipes (Esclavo)',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum amet, facere officia est maxime dolore libero sint perspiciatis suscipit consequatur! Fuga voluptatum fugiat saepe impedit? Veritatis voluptatum voluptas deserunt ut.',
+    requeriments: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt aliquam placeat aliquid quam voluptates illum vitae expedita. Dolor ad repellendus ab, corporis qui saepe quisquam, fugit ipsa odit numquam placeat.',
+    date: '666 de marzo del 2666'
+  },
+  {
+    title: 'Se buscan Felipes (Esclavo)',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum amet, facere officia est maxime dolore libero sint perspiciatis suscipit consequatur! Fuga voluptatum fugiat saepe impedit? Veritatis voluptatum voluptas deserunt ut.',
+    requeriments: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt aliquam placeat aliquid quam voluptates illum vitae expedita. Dolor ad repellendus ab, corporis qui saepe quisquam, fugit ipsa odit numquam placeat.',
+    date: '666 de marzo del 2666'
+  },
+  {
+    title: 'Se buscan Felipes (Esclavo)',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum amet, facere officia est maxime dolore libero sint perspiciatis suscipit consequatur! Fuga voluptatum fugiat saepe impedit? Veritatis voluptatum voluptas deserunt ut.',
+    requeriments: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt aliquam placeat aliquid quam voluptates illum vitae expedita. Dolor ad repellendus ab, corporis qui saepe quisquam, fugit ipsa odit numquam placeat.',
+    date: '666 de marzo del 2666'
+  },
+  {
+    title: 'Se buscan Felipes (Esclavo)',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum amet, facere officia est maxime dolore libero sint perspiciatis suscipit consequatur! Fuga voluptatum fugiat saepe impedit? Veritatis voluptatum voluptas deserunt ut.',
+    requeriments: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt aliquam placeat aliquid quam voluptates illum vitae expedita. Dolor ad repellendus ab, corporis qui saepe quisquam, fugit ipsa odit numquam placeat.',
+    date: '666 de marzo del 2666'
+  },
+  {
+    title: 'Se buscan Felipes (Esclavo)',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum amet, facere officia est maxime dolore libero sint perspiciatis suscipit consequatur! Fuga voluptatum fugiat saepe impedit? Veritatis voluptatum voluptas deserunt ut.',
+    requeriments: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt aliquam placeat aliquid quam voluptates illum vitae expedita. Dolor ad repellendus ab, corporis qui saepe quisquam, fugit ipsa odit numquam placeat.',
+    date: '666 de marzo del 2666'
+  },
+  {
+    title: 'Se buscan Felipes (Esclavo)',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum amet, facere officia est maxime dolore libero sint perspiciatis suscipit consequatur! Fuga voluptatum fugiat saepe impedit? Veritatis voluptatum voluptas deserunt ut.',
+    requeriments: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt aliquam placeat aliquid quam voluptates illum vitae expedita. Dolor ad repellendus ab, corporis qui saepe quisquam, fugit ipsa odit numquam placeat.',
+    date: '666 de marzo del 2666'
+  },
+  {
+    title: 'Se buscan Felipes (Esclavo)',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum amet, facere officia est maxime dolore libero sint perspiciatis suscipit consequatur! Fuga voluptatum fugiat saepe impedit? Veritatis voluptatum voluptas deserunt ut.',
+    requeriments: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt aliquam placeat aliquid quam voluptates illum vitae expedita. Dolor ad repellendus ab, corporis qui saepe quisquam, fugit ipsa odit numquam placeat.',
+    date: '666 de marzo del 2666'
+  },
+  {
+    title: 'Se buscan Felipes (Esclavo)',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum amet, facere officia est maxime dolore libero sint perspiciatis suscipit consequatur! Fuga voluptatum fugiat saepe impedit? Veritatis voluptatum voluptas deserunt ut.',
+    requeriments: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt aliquam placeat aliquid quam voluptates illum vitae expedita. Dolor ad repellendus ab, corporis qui saepe quisquam, fugit ipsa odit numquam placeat.',
+    date: '666 de marzo del 2666'
+  },
+  {
+    title: 'Se buscan Felipes (Esclavo)',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum amet, facere officia est maxime dolore libero sint perspiciatis suscipit consequatur! Fuga voluptatum fugiat saepe impedit? Veritatis voluptatum voluptas deserunt ut.',
+    requeriments: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt aliquam placeat aliquid quam voluptates illum vitae expedita. Dolor ad repellendus ab, corporis qui saepe quisquam, fugit ipsa odit numquam placeat.',
+    date: '666 de marzo del 2666'
+  },
+  {
+    title: 'Se buscan Felipes (Esclavo)',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum amet, facere officia est maxime dolore libero sint perspiciatis suscipit consequatur! Fuga voluptatum fugiat saepe impedit? Veritatis voluptatum voluptas deserunt ut.',
+    requeriments: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt aliquam placeat aliquid quam voluptates illum vitae expedita. Dolor ad repellendus ab, corporis qui saepe quisquam, fugit ipsa odit numquam placeat.',
+    date: '666 de marzo del 2666'
+  },
+  {
+    title: 'Se buscan Felipes (Esclavo)',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum amet, facere officia est maxime dolore libero sint perspiciatis suscipit consequatur! Fuga voluptatum fugiat saepe impedit? Veritatis voluptatum voluptas deserunt ut.',
+    requeriments: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt aliquam placeat aliquid quam voluptates illum vitae expedita. Dolor ad repellendus ab, corporis qui saepe quisquam, fugit ipsa odit numquam placeat.',
+    date: '666 de marzo del 2666'
+  },
+  {
+    title: 'Se buscan Felipes (Esclavo)',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum amet, facere officia est maxime dolore libero sint perspiciatis suscipit consequatur! Fuga voluptatum fugiat saepe impedit? Veritatis voluptatum voluptas deserunt ut.',
+    requeriments: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt aliquam placeat aliquid quam voluptates illum vitae expedita. Dolor ad repellendus ab, corporis qui saepe quisquam, fugit ipsa odit numquam placeat.',
+    date: '666 de marzo del 2666'
+  },
+  {
+    title: 'Se buscan Felipes (Esclavo)',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum amet, facere officia est maxime dolore libero sint perspiciatis suscipit consequatur! Fuga voluptatum fugiat saepe impedit? Veritatis voluptatum voluptas deserunt ut.',
+    requeriments: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt aliquam placeat aliquid quam voluptates illum vitae expedita. Dolor ad repellendus ab, corporis qui saepe quisquam, fugit ipsa odit numquam placeat.',
+    date: '666 de marzo del 2666'
+  },
+  
+]
 @Component({
   selector: 'app-offer',
   templateUrl: './offer.component.html',
   styleUrls: ['./offer.component.scss']
 })
+
+
+
 export class OfferComponent implements OnInit {
+
   @ViewChild(MatPaginator) paginator: MatPaginator;
+  dataSource: MatTableDataSource<JobOffer> = new MatTableDataSource<JobOffer>(DATA);
+  obs: Observable<any>;
 
   constructor(
-    public dialog: MatDialog
+    public dialog: MatDialog,
+    private changeDetectorRef: ChangeDetectorRef
   ) { }
 
   ngOnInit(): void {
+    this.changeDetectorRef.detectChanges();
+    this.dataSource.paginator = this.paginator;
+    this.obs = this.dataSource.connect();
   }
 
   moreDetails(job){
@@ -25,6 +131,12 @@ export class OfferComponent implements OnInit {
       minHeight: "50%",
       minWidth : "400px"
     });
+  }
+
+  ngOnDestroy(){
+    if (this.dataSource) { 
+      this.dataSource.disconnect(); 
+    }
   }
 
 }
