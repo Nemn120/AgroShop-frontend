07a59e8f5dbda7c56dafe5ba2ed97d5533bf2c80
Ivan <ivan.lopez1@unmsm.edu.pe>
2020-12-23 16:09:35 -0500

    Feat CREATE client-order-component on folder Order

---
+++ src/app/pages/admin/order/client-order-pending/client-order-pending.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-client-order-pending',
+  templateUrl: './client-order-pending.component.html',
+  styleUrls: ['./client-order-pending.component.scss']
+})
+export class ClientOrderPendingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
