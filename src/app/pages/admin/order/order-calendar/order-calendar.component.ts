fc81b944ff10f90d74fa884824b295be155691e3
Ivan <ivan.lopez1@unmsm.edu.pe>
2021-01-19 16:08:33 -0500

    Feat ADD hide-days on order-calendar.ts

--- src/app/pages/admin/order/order-calendar/order-calendar.component.ts
+++ src/app/pages/admin/order/order-calendar/order-calendar.component.ts
@@ -12,7 +12,27 @@ export class OrderCalendarComponent implements OnInit {
 
   ngOnInit(): void {
   }
-  calendarOptions: CalendarOptions = {
+  /*calendarOptions: CalendarOptions = {
     initialView: 'dayGridMonth'
   };
+  calendarOptions: CalendarOptions = {
+    initialView: 'dayGridMonth',
+    dateClick: this.handleDateClick.bind(this), // bind is important!
+    events: [
+      { title: 'event 1', date: '2019-04-01' },
+      { title: 'event 2', date: '2019-04-02' }
+    ]
+  };
+
+  handleDateClick(arg) {
+    alert('date click! ' + arg.dateStr)
+  }*/
+  calendarOptions: CalendarOptions = {
+    initialView: 'dayGridMonth',
+    weekends: true
+  };
+
+  toggleWeekends() {
+    this.calendarOptions.weekends = !this.calendarOptions.weekends // toggle the boolean!
+  }
 }
