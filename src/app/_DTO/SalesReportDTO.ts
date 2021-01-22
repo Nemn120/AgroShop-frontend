a6e1cd148a1fc3d921890613d3533593b1069b36
Ivan <ivan.lopez1@unmsm.edu.pe>
2021-01-22 16:37:02 -0500

    Feat ADD dtos on file _DTO

---
+++ src/app/_DTO/SalesReportDTO.ts
@@ -0,0 +1,7 @@
+export class SalesReportDTO{
+    farmer : number;
+    status : string;
+    dateIni : Date;
+    dateFin : Date;
+    ids: Array<Number>;
+} 
---
+++ src/app/_DTO/SalesReportResponseDTO.ts
@@ -0,0 +1,6 @@
+export class SalesReportResponseDTO{
+    total : Number;
+    count : Number;
+    name: String;
+    price: Number;
+} 
