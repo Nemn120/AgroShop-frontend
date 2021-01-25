0605211c451c95ae403648b1551a168d9437393e
Ivan <ivan.lopez1@unmsm.edu.pe>
2021-01-25 17:29:42 -0500

    Feat ADD orderPendings on order-calendar.ts

--- src/app/pages/admin/order/order-calendar/order-calendar.component.ts
+++ src/app/pages/admin/order/order-calendar/order-calendar.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { CalendarOptions } from '@fullcalendar/angular'; // useful for typechecking
+import { MatDialog } from '@angular/material/dialog';
+import { CalendarOptions } from '@fullcalendar/angular'; 
+import { RestService } from 'src/app/_service/rest.service';
+import { SharedService } from 'src/app/_service/shared.service';
 
 @Component({
   selector: 'app-order-calendar',
@@ -8,13 +11,18 @@ import { CalendarOptions } from '@fullcalendar/angular'; // useful for typecheck
 })
 export class OrderCalendarComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private restService: RestService,
+    public dialog: MatDialog,
+    public sharedService: SharedService
+  ) { }
 
   ngOnInit(): void {
+    this.listData();
   }
   calendarOptions: CalendarOptions = {
     initialView: 'dayGridMonth',
-    dateClick: this.handleDateClick.bind(this), // bind is important!
+    dateClick: this.handleDateClick.bind(this), 
     weekends:true,
     events: [
       { title: 'Pedido 1', date: '2021-01-21' },
@@ -31,6 +39,22 @@ export class OrderCalendarComponent implements OnInit {
   }
 
   toggleWeekends() {
-    this.calendarOptions.weekends = !this.calendarOptions.weekends // toggle the boolean!
+    this.calendarOptions.weekends = !this.calendarOptions.weekends 
+  }
+
+  //carga los pedidos pendientes
+  listData(){
+    let param = {
+      id: this.sharedService.userSession.id,
+      data: {
+        status:"Pendiente"
+      }
+    };
+    this.restService.requestApiRestData('order/gobsf', param)
+      .subscribe( result => {
+        
+        console.log(result);
+      }
+      );
   }
 }
